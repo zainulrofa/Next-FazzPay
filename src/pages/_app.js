90ca0295e3d7cd05b81c3998b52ca3c7eaed2230
rofa <zainrofa29@gmail.com>
2022-11-25 22:28:00 +0700

    set up redux

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import store, { persistedStore } from "src/redux/store";
+import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        closeOnClick={true}
+        pauseOnHover={true}
+        draggable={true}
+        theme="light"
+      />
+      <Provider store={store}>
+        <PersistGate persistor={persistedStore} loading={null}>
+          <Component {...pageProps} />;
+        </PersistGate>
+      </Provider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
