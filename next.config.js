576becef8af7b65adb887e8868ff735ccc32dd0d
rofa <zainrofa29@gmail.com>
2022-11-26 23:12:33 +0700

    set cloudinary

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    CLOUDINARY_LINK: process.env.CLOUDINARY_LINK,
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
