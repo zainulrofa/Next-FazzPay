7798422b2b603cafa41bf91747cec08ace385adc
rofa <zainrofa29@gmail.com>
2022-11-26 23:24:33 +0700

    add history reducer

---
+++ src/redux/reducers/history.js
@@ -0,0 +1,44 @@
+import { ActionType } from "redux-promise-middleware";
+import { ACTION_STRING } from "../actions/actionStrings";
+
+const initialState = {
+  isLoading: false,
+  isFulfilled: false,
+  isError: false,
+  error: null,
+  history: [],
+};
+
+const historyReducer = (prevState = initialState, { type, payload }) => {
+  const { Pending, Rejected, Fulfilled } = ActionType;
+  const { transactionGetHistory } = ACTION_STRING;
+
+  switch (type) {
+    case transactionGetHistory.concat("_", Pending):
+      return {
+        ...prevState,
+        isLoading: true,
+        isError: false,
+        isFulfilled: false,
+      };
+    case transactionGetHistory.concat("_", Rejected):
+      return {
+        ...prevState,
+        isError: true,
+        isLoading: false,
+        isFulfilled: false,
+        error: payload.error.response.data.msg,
+      };
+    case transactionGetHistory.concat("_", Fulfilled):
+      return {
+        ...prevState,
+        isFulfilled: true,
+        isLoading: false,
+        history: payload.data.data,
+      };
+    default:
+      return prevState;
+  }
+};
+
+export default historyReducer;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./auth";
+import historyReducer from "./history";
 import userReducer from "./user";
 
 export default combineReducers({
   auth: authReducer,
   user: userReducer,
+  history: historyReducer,
 });
