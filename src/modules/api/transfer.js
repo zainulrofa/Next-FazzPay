3a00406ae465f808a55ba518ddeee8b2135054b1
rofa <zainrofa29@gmail.com>
2022-11-29 03:26:25 +0700

    implementation transfer

---
+++ src/modules/api/transfer.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const config = (token) => {
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+};
+
+export const getAllUser = (token, page, search) => {
+  const URL = !search
+    ? `${process.env.NEXT_PUBLIC_BACKEND_URL}/user?limit=6&sort=firstName%20DESC&page=${page}`
+    : `${process.env.NEXT_PUBLIC_BACKEND_URL}/user?limit=6&search=${search}&sort=firstName%20DESC&page=${page}`;
+  return axios.get(URL, config(token));
+};
+
+export const transfer = (token, body) => {
+  const URL = `${process.env.NEXT_PUBLIC_BACKEND_URL}/transaction/transfer`;
+  return axios.post(URL, body, config(token));
+};
