f34e6fba189a76e04790e3f6917f914c4ef9217f
rofa <zainrofa29@gmail.com>
2022-11-29 03:31:41 +0700

    add transfer action

---
+++ src/redux/reducers/transfer.js
@@ -0,0 +1,74 @@
+import { ACTION_STRING } from "src/redux/actions/actionStrings";
+
+const initialState = {
+  transferData: {
+    receiverId: "",
+    amount: "",
+    notes: "",
+    date: "",
+    receiverData: null,
+  },
+  transferResult: {
+    isLoading: false,
+    isFulfilled: false,
+    isError: false,
+    status: 0,
+    msg: "",
+    data: null,
+    error: "",
+  },
+};
+
+const transferReducer = (prevState = initialState, { payload, type }) => {
+  switch (type) {
+    case ACTION_STRING.resetTransfer:
+      return initialState;
+
+    case ACTION_STRING.transferData:
+      return {
+        ...prevState,
+        transferData: {
+          receiverId: payload.receiverId,
+          date: payload.date,
+          amount: payload.amount,
+          notes: payload.notes,
+          receiverData: payload.receiverData,
+        },
+      };
+
+    case ACTION_STRING.transfer.concat(ACTION_STRING.pending):
+      return {
+        ...prevState,
+        transferResult: {
+          isLoading: true,
+          isFulfilled: false,
+          isError: false,
+        },
+      };
+    case ACTION_STRING.transfer.concat(ACTION_STRING.rejected):
+      return {
+        ...prevState,
+        transferResult: {
+          isLoading: false,
+          isFulfilled: false,
+          isError: true,
+          error: payload.error.response.data.msg,
+        },
+      };
+    case ACTION_STRING.transfer.concat(ACTION_STRING.fulfilled):
+      return {
+        ...prevState,
+        transferResult: {
+          isLoading: false,
+          isFulfilled: true,
+          status: payload.data.status,
+          data: payload.data.data,
+          msg: payload.data.msg,
+        },
+      };
+    default:
+      return initialState;
+  }
+};
+
+export default transferReducer;
