b116460c3db8c2a30658d92c4a365196223d1f1c
rofa <zainrofa29@gmail.com>
2022-11-29 03:29:58 +0700

    add action transfer

---
+++ src/redux/actions/transfer.js
@@ -0,0 +1,48 @@
+import { transfer } from "src/modules/api/transfer";
+import { ACTION_STRING } from "./actionStrings";
+
+const transferPending = () => {
+  return {
+    type: ACTION_STRING.transfer.concat(ACTION_STRING.pending),
+  };
+};
+const transferRejected = (error) => {
+  return {
+    type: ACTION_STRING.transfer.concat(ACTION_STRING.rejected),
+    payload: { error },
+  };
+};
+const transferFulfilled = (data) => {
+  return {
+    type: ACTION_STRING.transfer.concat(ACTION_STRING.fulfilled),
+    payload: { data },
+  };
+};
+
+const transferThunk = (token, body) => {
+  return async (dispatch) => {
+    try {
+      dispatch(transferPending());
+      const result = await transfer(token, body);
+      dispatch(transferFulfilled(result.data));
+    } catch (error) {
+      dispatch(transferRejected(error));
+    }
+  };
+};
+
+const transferReset = () => {
+  return {
+    type: ACTION_STRING.resetTransfer,
+  };
+};
+
+const transferData = (body) => {
+  return {
+    type: ACTION_STRING.transferData,
+    payload: { body },
+  };
+};
+const transferAction = { transferReset, transferData, transferThunk };
+
+export default transferAction;
